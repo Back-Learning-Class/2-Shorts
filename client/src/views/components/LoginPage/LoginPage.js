7738ce771aafbb5eeca5ebd8c23f425fa041d3ad
jeonjinwoo <hp5234@naver.com>
2021-07-30 11:12:55 +0900

    ADD Page Folder

---
+++ client/src/views/components/LoginPage/LoginPage.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+
+function LoginPage() {
+    return (
+        <div>
+            // 아이디(이메일형식 )
+            <inputId></inputId>
+            // 비밀번호 
+            <inputPasswd></inputPasswd>
+        </div>
+    );
+}
+
+function inputId(){
+    
+    return(
+        <div>
+            id : <input id="inputId" type="email"></input>
+        </div>
+    );
+}
+
+function inputPasswd(){
+    return(
+        <div>
+            password : <input id="inputPasswd" type="password"></input>
+        </div>
+    );
+};
+
+function LoginButton(){
+
+    return(
+        <div>
+            <input type="button" onClick="">Login</input>
+            <input type="button" onClick="">Sign up</input>
+        </div>
+    );
+}
+
+
+export default LoginPage;
