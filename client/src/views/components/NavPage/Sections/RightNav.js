bcb910284ca1a08c9625f577f2c61f11850bca20
J-vInyL <wnstjq7894@gmail.com>
2021-08-21 23:51:04 +0900

    EDIT : show & hide - loginlogout

--- client/src/views/components/NavPage/Sections/RightNav.js
+++ client/src/views/components/NavPage/Sections/RightNav.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Menu } from "antd";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
 
 function RightNav(props) {
+  const [Access, setAccess] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/route/status", {
+        withCredentials: true
+      })
+      .then(response => {
+        setAccess(response.data.isAuth);
+      });
+  }, [Access]);
+
   const logoutHandler = () => {
     axios.get("http://localhost:5000/api/route/logout").then(response => {
       if (response.status === 200) {
+        console.log("logout test", response.data.isAuth);
         props.history.push("/login");
       } else {
         alert("Log Out Failed");
@@ -14,19 +27,29 @@ function RightNav(props) {
     });
   };
 
-  return (
-    <Menu mode={props.mode}>
-      <Menu.Item key="mail">
-        <a href="/login">로그인</a>
-      </Menu.Item>
-      <Menu.Item key="app">
-        <a href="/Signup">회원가입</a>
-      </Menu.Item>
-      <Menu.Item key="logout">
-        <a onClick={logoutHandler}>로그아웃</a>
-      </Menu.Item>
-    </Menu>
-  );
+  if (!Access) {
+    return (
+      <Menu mode={props.mode}>
+        <Menu.Item key="mail">
+          <a href="/login">로그인</a>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <a href="/Signup">회원가입</a>
+        </Menu.Item>
+      </Menu>
+    );
+  } else {
+    return (
+      <Menu>
+        <Menu.Item key="logout">
+          <a onClick={logoutHandler}>로그아웃</a>
+        </Menu.Item>
+        <Menu.Item>
+          <h1>사용자</h1>
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
 export default withRouter(RightNav);
