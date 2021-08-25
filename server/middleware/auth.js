fe9917af7391c32c123363cb609068fb17b070c3
J-vInyL <wnstjq7894@gmail.com>
2021-08-25 22:17:26 +0900

    EDIT : signupPage / auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,30 +5,30 @@ let auth = (req, res, next) => {
   let accessToken = req.cookies.w_auth;
   //let test = req.header.cookie;
   //클라이언트 쿠키에서 토큰을 가져온다
-  console.log("accesstoken", accessToken);
   if (accessToken) {
     jwt.verify(accessToken, "sEcReAt", async function (err, decode) {
       try {
         let finduser = await User.findAll({
           attributes: ["email"],
           where: {
-            email: decode
+            email: decode.tokenId
           }
         });
-        if (finduser[0].dataValues.email != decode) {
+        if (finduser[0].dataValues.email != decode.tokenId) {
           return res.json({
             isAuth: false,
             error: true
           });
         }
-
-        req.user = decode;
+        req.user = decode.tokenId;
         console.log("middle auth", req.user);
         next(); //next를 해줘야  라우터에 다음 부분들을 실행시켜줌
       } catch (error) {
         console.log("에러발생 : moddieware /auth", error);
       }
     });
+
+    //여기서 토큰 검사 및 재발급
   }
 };
 
